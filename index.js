91e939bdc50bbad00486e3d5158b9e23246dc163
Robin de Mourat <robin.demourat@gmail.com>
2017-12-21 09:28:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+  Asset: require('./asset.schema.json'),
+  User: require('./user.schema.json'),
+  Resource: require('./resource.schema.json'),
+  Composition: require('./composition.schema.json'),
+}
